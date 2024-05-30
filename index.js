c84ab6f27121e4d9684a390e06c5bf02c2a10e4b
amanc34048 <amanc34048@gmail.com>
2024-05-30 16:30:52 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./routes')
 
 const app=express()
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:process.env.FRONTEND_URL||FRONTEND_URL,
     credentials:true
 }))
 app.use(express.json())
